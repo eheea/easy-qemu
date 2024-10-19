a4eb8adca6df2fa354b4e282550690c7412c24b8
eheea <eheea3333@gmail.com>
2024-10-19 13:57:37 +0300

    clean output

--- fedora.arch/post-restart.sh
+++ fedora.arch/post-restart.sh
@@ -6,9 +6,9 @@ echo "export LIBVIRT_DEFAULT_URI='qemu:///system'" >> ~/.bashrc
 source ~/.bashrc
 
 #giving permissions on the default directory
-sudo setfacl -R -b /var/lib/libvirt/images >> /dev/null
-sudo setfacl -R -m u:"$USER":rwX /var/lib/libvirt/images >> /dev/null
-sudo setfacl -m d:u:"$USER":rwx /var/lib/libvirt/images >> /dev/null
+sudo setfacl -R -b /var/lib/libvirt/images  > /dev/null
+sudo setfacl -R -m u:"$USER":rwX /var/lib/libvirt/images > /dev/null
+sudo setfacl -m d:u:"$USER":rwx /var/lib/libvirt/images > /dev/null
 
 #setting up default network
 sudo virsh net-start default
--- ubuntu/qemu.sh
+++ ubuntu/qemu.sh
@@ -32,13 +32,13 @@ echo "export LIBVIRT_DEFAULT_URI='qemu:///system'" >> ~/.bashrc
 source ~/.bashrc
 
 #giving permissions on the default directory
-sudo setfacl -R -b /var/lib/libvirt/images
-sudo setfacl -R -m u:"$USER":rwX /var/lib/libvirt/images
-sudo setfacl -m d:u:"$USER":rwx /var/lib/libvirt/images
+sudo setfacl -R -b /var/lib/libvirt/images  > /dev/null
+sudo setfacl -R -m u:"$USER":rwX /var/lib/libvirt/images > /dev/null
+sudo setfacl -m d:u:"$USER":rwx /var/lib/libvirt/images > /dev/null
 
 #setting up default network
-sudo virsh net-start default
-sudo virsh net-autostart default
+sudo virsh net-start default > /dev/null
+sudo virsh net-autostart default > /dev/null
 echo "                       "
 echo -e "\e[32mit is recommened to reboot, reboot now?\e[0m"
 echo "1) yes"
